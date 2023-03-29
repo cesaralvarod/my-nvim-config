16a6e3bfc77fdf7f2f01dd8e769576a7f69847fe
cesaralvarod <cesaralvarod@gmail.com>
2023-03-29 00:40:38 -0500

    migrate to lazy.nvim

--- init.lua
+++ init.lua
@@ -2,9 +2,6 @@ require("cesaralvarod.settings")
 
 require("cesaralvarod.globals")
 
-require("cesaralvarod.plugins")
-require("cesaralvarod.load-plugins")
-
-require("cesaralvarod.themes.tokyonight")
-
 require("cesaralvarod.keymappings")
+
+require("cesaralvarod.lazy")
